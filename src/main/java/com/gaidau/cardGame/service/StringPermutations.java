c6f2f602e346b92083d0758012f4f1f4af40132f
MihaiGaidau <gaidaumihai@gmail.com>
2019-12-10 11:35:47 +0200

    issue resolved

--- src/main/java/com/gaidau/cardGame/service/StringPermutations.java
+++ src/main/java/com/gaidau/cardGame/service/StringPermutations.java
@@ -1,5 +1,9 @@
 package com.gaidau.cardGame.service;
 
+
+import lombok.extern.log4j.Log4j2;
+
+@Log4j2
 public class StringPermutations {
 
     public static void permutation(String str) {
@@ -8,7 +12,7 @@ public class StringPermutations {
 
     private static void permutation(String prefix, String str) {
         int n = str.length();
-        if (n == 0) System.out.println(prefix);
+        if (n == 0) log.debug(prefix);
         else {
             for (int i = 0; i < n; i++)
                 permutation(prefix + str.charAt(i), str.substring(0, i) + str.substring(i+1, n));
